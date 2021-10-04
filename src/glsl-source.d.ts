4ebce99b362d7ca07d8b3898fa01408ad66cd608
Rodney Folz <rodney@nextdoor.com>
2021-10-03 17:36:52 -0700

    Add koch snowflake uv transform

--- src/glsl-source.d.ts
+++ src/glsl-source.d.ts
@@ -24,7 +24,7 @@ declare class GlslSource {
     compile(transforms: TransformApplication[]): {
         frag: string;
         uniforms: {
-            [x: string]: string | ((context: any, props: any) => number | number[]) | import("regl").Texture2D | import("regl").Uniform | undefined;
+            [x: string]: string | import("regl").Texture2D | ((context: any, props: any) => number | number[]) | import("regl").Uniform | undefined;
         };
     };
 }
