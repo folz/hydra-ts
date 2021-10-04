c3b342aeb74485d76574a1f6e2eacff21f71b10d
Rodney Folz <rodney@nextdoor.com>
2021-10-04 09:31:15 -0700

    processGlsl snapshot tests

---
+++ src/generator-factory.test.js
@@ -0,0 +1,10 @@
+import { transforms } from './glsl/glsl-functions';
+import { processGlsl } from './generator-factory';
+test.each(transforms)('processGlsl($definition.name)', (definition) => {
+    expect(definition).toMatchSnapshot('initial');
+    const processed = processGlsl(definition);
+    expect(processed).toMatchSnapshot('processed');
+    // processGlsl mutates definition inputs, so capture that change in test too.
+    const processed2 = processGlsl(definition);
+    expect(processed2).toMatchSnapshot('processed+mutated');
+});
