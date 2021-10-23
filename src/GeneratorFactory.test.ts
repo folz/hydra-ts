5ff033e30430f098b7f39942bb4eb2dc1b5ba95c
Rodney Folz <rodney@nextdoor.com>
2021-10-23 15:08:39 -0700

    Update GeneratorFactory tests to reflect no longer mutating input array

--- src/GeneratorFactory.test.ts
+++ src/GeneratorFactory.test.ts
@@ -7,8 +7,4 @@ test.each(transforms)('processGlsl($definition.name)', (definition) => {
   const processed = processGlsl(definition);
 
   expect(processed).toMatchSnapshot('processed');
-
-  // processGlsl mutates definition inputs, so capture that change in test too.
-  const processed2 = processGlsl(definition);
-  expect(processed2).toMatchSnapshot('processed+mutated');
 });
